f113990f6c53bf70e033a244aa75fe5cf7675a9c
Eslam Elfateh <eslam.elfateh.94@gmail.com>
2020-10-25 19:55:10 +0200

    add carrousel in landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './resources/styles.css';
 
-import Header from './component/header-footer/header'
+import Header from './component/header-footer/header';
+import Featured from './component/featured/index'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{height:'1500px', background:'black'}}>
       <Header/>
+      <Featured/>
     </div>
   );
 }
